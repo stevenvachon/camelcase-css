cb3b8d8fad4609d78ce54b10aa35fa3ab4c55921
Steven D Vachon <contact@svachon.com>
2015-08-28 09:25:35 -0400

    v1.0.0

---
+++ test.js
@@ -0,0 +1,16 @@
+"use strict";
+var camelCaseCSS = require("./");
+
+var expect = require("chai").expect;
+
+
+
+it("should work", function(done)
+{
+	expect( camelCaseCSS("-webkit-border-radius") ).to.equal("WebkitBorderRadius");
+	expect( camelCaseCSS("-moz-border-radius")    ).to.equal("MozBorderRadius");
+	expect( camelCaseCSS("-ms-border-radius")     ).to.equal("msBorderRadius");
+	expect( camelCaseCSS("border-radius")         ).to.equal("borderRadius");
+	
+	done();
+});
