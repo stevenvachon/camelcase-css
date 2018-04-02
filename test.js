79a05e936a17ee39b6a58ecb257ed52dcc427778
David Clark <dclark@mapbox.com>
2018-04-02 08:25:20 -0700

    Fix spacing

--- test.js
+++ test.js
@@ -11,9 +11,9 @@ it("should work", function(done)
 	expect( camelCaseCSS("-WEBKIT-BORDER-RADIUS") ).to.equal("WebkitBorderRadius");
 	expect( camelCaseCSS("-webkIT-borDer-rADIUS") ).to.equal("WebkitBorderRadius");
 	expect( camelCaseCSS("-moz-border-radius")    ).to.equal("MozBorderRadius");
-	expect( camelCaseCSS("-MOZ-BORDER-RADIUS") ).to.equal("MozBorderRadius");
+	expect( camelCaseCSS("-MOZ-BORDER-RADIUS")    ).to.equal("MozBorderRadius");
 	expect( camelCaseCSS("-ms-border-radius")     ).to.equal("msBorderRadius");
-	expect( camelCaseCSS("-MS-BORDER-RADIUS") ).to.equal("msBorderRadius");
+	expect( camelCaseCSS("-MS-BORDER-RADIUS")     ).to.equal("msBorderRadius");
 	expect( camelCaseCSS("border-radius")         ).to.equal("borderRadius");
 	expect( camelCaseCSS("BORDER-RADIUS")         ).to.equal("borderRadius");
 	
