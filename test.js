d70ad043af8560f73780e672505d078c4daddfdc
David Clark <david.dave.clark@gmail.com>
2018-04-02 09:21:36 -0700

    Handle uppercase CSS property names (#1)

--- test.js
+++ test.js
@@ -8,9 +8,14 @@ var expect = require("chai").expect;
 it("should work", function(done)
 {
 	expect( camelCaseCSS("-webkit-border-radius") ).to.equal("WebkitBorderRadius");
+	expect( camelCaseCSS("-WEBKIT-BORDER-RADIUS") ).to.equal("WebkitBorderRadius");
+	expect( camelCaseCSS("-webkIT-borDer-rADIUS") ).to.equal("WebkitBorderRadius");
 	expect( camelCaseCSS("-moz-border-radius")    ).to.equal("MozBorderRadius");
+	expect( camelCaseCSS("-MOZ-BORDER-RADIUS")    ).to.equal("MozBorderRadius");
 	expect( camelCaseCSS("-ms-border-radius")     ).to.equal("msBorderRadius");
+	expect( camelCaseCSS("-MS-BORDER-RADIUS")     ).to.equal("msBorderRadius");
 	expect( camelCaseCSS("border-radius")         ).to.equal("borderRadius");
+	expect( camelCaseCSS("BORDER-RADIUS")         ).to.equal("borderRadius");
 	
 	expect( camelCaseCSS("float") ).to.equal("cssFloat");
 	
